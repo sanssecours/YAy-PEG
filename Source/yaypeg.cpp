25a6bcf6be15182197945449c409277506552d7d
René Schwaiger <sanssecours@me.com>
2018-10-03 13:18:26 +0200

    Main: Add initial version of parser

--- Source/yaypeg.cpp
+++ Source/yaypeg.cpp
@@ -1,58 +1,44 @@
-// Copyright (c) 2014-2018 Dr. Colin Hirsch and Daniel Frey
-// Please see LICENSE for license or visit https://github.com/taocpp/PEGTL/
+// -- Imports ------------------------------------------------------------------
 
 #include <iostream>
 #include <string>
 
 #include <tao/pegtl.hpp>
 
+using std::cout;
+
 namespace pegtl = tao::pegtl;
 
-namespace hello {
-// Parsing rule that matches a literal "Hello, ".
+using pegtl::argv_input;
+using pegtl::eof;
+using pegtl::identifier;
+using pegtl::nothing;
+using pegtl::parse;
+using pegtl::until;
 
-struct prefix : pegtl::string<'H', 'e', 'l', 'l', 'o', ',', ' '> {};
+// -- Grammar ------------------------------------------------------------------
 
-// Parsing rule that matches a non-empty sequence of
-// alphabetic ascii-characters with greedy-matching.
+namespace yaypeg {
 
-struct name : pegtl::plus<pegtl::alpha> {};
+struct plain_scalar : identifier {};
 
-// Parsing rule that matches a sequence of the 'prefix'
-// rule, the 'name' rule, a literal "!", and 'eof'
-// (end-of-file/input), and that throws an exception
-// on failure.
+struct yaml : until<plain_scalar, eof> {};
 
-struct grammar : pegtl::must<prefix, name, pegtl::one<'!'>, pegtl::eof> {};
+template <typename Rule> struct action : nothing<Rule> {};
 
-// Class template for user-defined actions that does
-// nothing by default.
-
-template <typename Rule> struct action : pegtl::nothing<Rule> {};
-
-// Specialisation of the user-defined action to do
-// something when the 'name' rule succeeds; is called
-// with the portion of the input that matched the rule.
-
-template <> struct action<name> {
-  template <typename Input> static void apply(const Input &in, std::string &v) {
-    v = in.string();
+template <> struct action<plain_scalar> {
+  template <typename Input> static void apply(const Input &input) {
+    cout << "Found plain scalar “" << input.string() << "”";
   }
 };
 
-} // namespace hello
+} // namespace yaypeg
+
+// -- Main ---------------------------------------------------------------------
 
 int main(int argc, char *argv[]) {
   if (argc > 1) {
-    // Start a parsing run of argv[1] with the string
-    // variable 'name' as additional argument to the
-    // action; then print what the action put there.
-
-    std::string name;
-
-    pegtl::argv_input<> in(argv, 1);
-    pegtl::parse<hello::grammar, hello::action>(in, name);
-
-    std::cout << "Good bye, " << name << "!" << std::endl;
+    argv_input<> input{argv, 1};
+    parse<yaypeg::yaml, yaypeg::action>(input);
   }
 }
