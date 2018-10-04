7c2264ae644fa5616dcca778d9f72ac7de204149
René Schwaiger <sanssecours@me.com>
2018-10-04 20:58:13 +0200

    Parser: Add first version of conversion function

---
+++ Source/convert.cpp
@@ -0,0 +1,53 @@
+/**
+ * @file
+ *
+ * @brief This file contains a function to convert a YAML file to a key set.
+ *
+ * @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
+ */
+
+// -- Imports ------------------------------------------------------------------
+
+#include "convert.hpp"
+#include "grammar.hpp"
+
+using std::string;
+
+using tao::pegtl::file_input;
+using tao::pegtl::parse;
+
+using CppKey = kdb::Key;
+using CppKeySet = kdb::KeySet;
+
+namespace yaypeg {
+
+// -- Function -----------------------------------------------------------------
+
+/**
+ * @brief This function converts the given YAML file to keys and adds the
+ *        result to `keySet`.
+ *
+ * @param keySet The function adds the converted keys to this variable.
+ * @param parent The function uses this parent key of `keySet` to emit error
+ *               information.
+ * @param filename This parameter stores the path of the YAML file this
+ *                 function converts.
+ *
+ * @retval  0 if parsing was successful and the function did not change the
+ *            given keyset
+ * @retval  1 if parsing was successful and the function did change `keySet`
+ */
+int addToKeySet(CppKeySet &keySet, CppKey &parent __attribute__((unused)),
+                string const &filename) {
+  file_input<> input{filename};
+  parse<yaml, action>(input);
+
+  CppKeySet keys{};
+  int status = (keys.size() <= 0) ? 0 : 1;
+
+  keySet.append(keys);
+
+  return status;
+}
+
+} // namespace yaypeg
--- Source/yaypeg.cpp
+++ Source/yaypeg.cpp
@@ -5,20 +5,22 @@
 
 #include <tao/pegtl.hpp>
 
-#include "grammar.hpp"
+#include <kdb.hpp>
+
+#include "convert.hpp"
 
 using std::cerr;
 using std::endl;
+using std::string;
 
-namespace pegtl = tao::pegtl;
-
-using pegtl::file_input;
-using pegtl::parse;
 using tao::pegtl::input_error;
 using tao::pegtl::parse_error;
 
-using yaypeg::action;
-using yaypeg::yaml;
+using ckdb::keyNew;
+using kdb::Key;
+using kdb::KeySet;
+
+using yaypeg::addToKeySet;
 
 // -- Main ---------------------------------------------------------------------
 
@@ -29,9 +31,12 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
+  string filename = argv[1];
+  KeySet keys;
+  Key parent{keyNew("user", KEY_END, "", KEY_VALUE)};
+
   try {
-    file_input<> input{argv[1]};
-    parse<yaml, action>(input);
+    addToKeySet(keys, parent, filename);
   } catch (input_error const &error) {
     cerr << "Unable to open input: " << error.what() << endl;
   } catch (parse_error const &error) {
