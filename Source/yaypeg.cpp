32f96d3e16565a582defb28c56c35fe6b88490d1
René Schwaiger <sanssecours@me.com>
2018-10-04 20:10:01 +0200

    Main: Require at least one argument

--- Source/yaypeg.cpp
+++ Source/yaypeg.cpp
@@ -23,14 +23,18 @@ using yaypeg::yaml;
 // -- Main ---------------------------------------------------------------------
 
 int main(int argc, char *argv[]) {
-  if (argc > 1) {
-    try {
-      file_input<> input{argv[1]};
-      parse<yaml, action>(input);
-    } catch (input_error const &error) {
-      cerr << "Unable to open input: " << error.what() << endl;
-    } catch (parse_error const &error) {
-      cerr << "Unable to parser input: " << error.what() << endl;
-    }
+
+  if (argc != 2) {
+    cerr << "Usage: " << argv[0] << " filename" << endl;
+    return EXIT_FAILURE;
+  }
+
+  try {
+    file_input<> input{argv[1]};
+    parse<yaml, action>(input);
+  } catch (input_error const &error) {
+    cerr << "Unable to open input: " << error.what() << endl;
+  } catch (parse_error const &error) {
+    cerr << "Unable to parser input: " << error.what() << endl;
   }
 }
