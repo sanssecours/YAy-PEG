f92561fc109ead9ec9a05694653fa7d54699798a
René Schwaiger <sanssecours@me.com>
2018-12-01 13:21:38 +0100

    Convert: Handle syntax errors

--- Source/convert.cpp
+++ Source/convert.cpp
@@ -39,18 +39,29 @@ using tao::yaypeg::parse;
  * @param filename This parameter stores the path of the YAML file this
  *                 function converts.
  *
+ * @retval -1 if there was a error converting the YAML file
  * @retval  0 if parsing was successful and the function did not change the
  *            given keyset
  * @retval  1 if parsing was successful and the function did change `keySet`
  */
 int addToKeySet(CppKeySet &keySet, CppKey &parent, string const &filename) {
+  using std::cerr;
+  using std::endl;
+  using tao::yaypeg::parse_error;
+
   State state{parent};
 
   // Check grammar for problematic code
   analyze<yaml>();
 
   file_input<> input{filename};
-  parse<yaml, action>(input, state);
+
+  try {
+    parse<yaml, action>(input, state);
+  } catch (parse_error const &error) {
+    cerr << error.what() << endl;
+    return -1;
+  }
 
   CppKeySet keys = state.getKeySet();
 
