6de07cc73c2ce882bfdf838a98941f7f7e2ad61b
René Schwaiger <sanssecours@me.com>
2018-12-18 14:25:56 +0100

    Parser: Remove large parts of grammar

--- Source/grammar.hpp
+++ Source/grammar.hpp
@@ -73,72 +73,6 @@ using tao::yaypeg::utf8::ranges;
 // = Grammar =
 // ===========
 
-// [1]
-struct c_printable
-    : sor<one<'\t', '\n', '\r', 0x85>,
-          ranges<' ', 0x7E, 0xA0, 0xD7FF, 0xE000, 0xFFFD, 0x10000, 0x10FFFF>> {
-};
-// [3]
-struct c_byte_order_mark : one<0xFEFF> {};
-
-// [22]
-struct c_indicator : one<'-', '?', ':', ',', '[', ']', '{', '}', '#', '&', '*',
-                         '!', ',', '>', '\'', '"', '%', '@', '`'> {};
-// [23]
-struct c_flow_indicator : one<',', '[', ']', '{', '}'> {};
-
-// [24]
-struct b_line_feed : one<'\n'> {};
-// [25]
-struct b_carriage_return : one<'\r'> {};
-// [26]
-struct b_char : sor<b_line_feed, b_carriage_return> {};
-// [27]
-struct nb_char : seq<not_at<sor<b_char, c_byte_order_mark>>, c_printable> {};
-// [28]
-struct b_break
-    : sor<seq<b_carriage_return, b_line_feed>, b_carriage_return, b_line_feed> {
-};
-
-// [30]
-struct b_non_content : b_break {};
-
-// [31]
-struct s_space : one<' '> {};
-// [32]
-struct s_tab : one<'\t'> {};
-// [33]
-struct s_white : sor<s_space, s_tab> {};
-// [34]
-struct ns_char : seq<not_at<s_white>, nb_char> {};
-
-// [63]
-template <size_t repetitions> struct s_indent : rep<repetitions, s_space> {};
-
-template <size_t whitespaces> struct s_indent_plus {
-  using analyze_t =
-      tao::yaypeg::analysis::generic<tao::yaypeg::analysis::rule_type::ANY>;
-
-  template <tao::yaypeg::apply_mode, tao::yaypeg::rewind_mode,
-            template <typename...> class, template <typename...> class,
-            typename Input>
-  static bool match(Input &input, State &state) {
-    if (input.size(whitespaces) <= whitespaces) {
-      return false;
-    }
-    auto end = input.begin() + (whitespaces - state.indentation.back());
-    for (auto current = input.begin(); current != end; current++) {
-      LOGF("Current: “{}”", *current);
-      if (*current != ' ') {
-        return false;
-      }
-    }
-    input.bump(whitespaces);
-    state.indentation.push_back(whitespaces);
-    return true;
-  }
-};
-
 struct push_indent {
   using analyze_t =
       tao::yaypeg::analysis::generic<tao::yaypeg::analysis::rule_type::ANY>;
@@ -147,7 +81,6 @@ struct push_indent {
             template <typename...> class, template <typename...> class,
             typename Input>
   static bool match(Input &input, State &state) {
-    // TODO: Check for non-empty line
     size_t indent = 0;
     for (auto current = input.begin();
          input.size(indent + 1) >= indent + 1 && *current == ' '; ++current) {
@@ -164,56 +97,6 @@ struct push_indent {
 
 struct pop_indent : success {};
 
-// [66]
-struct s_separate_in_line : sor<plus<s_white>, bol> {};
-
-// [126]
-struct ns_plain_safe;
-struct ns_plain_first : sor<seq<not_at<c_indicator>, ns_char>,
-                            one<'?', ':', '-'>, ns_plain_safe> {};
-
-// [128]
-struct ns_plain_safe_out : ns_char {};
-// [129]
-struct ns_plain_safe_in : seq<not_at<c_flow_indicator>, ns_char> {};
-
-// [127]
-struct ns_plain_safe {
-  template <tao::yaypeg::apply_mode ApplyMode,
-            tao::yaypeg::rewind_mode RewindMode,
-            template <typename...> class Action,
-            template <typename...> class Control, typename Input>
-  static bool match(Input &input, State &state) {
-    if (state.contextFlowOut() || state.contextBlockKey()) {
-      return ns_plain_safe_out::match<ApplyMode, RewindMode, Action, Control>(
-          input, state);
-    }
-    return ns_plain_safe_in::match<ApplyMode, RewindMode, Action, Control>(
-        input, state);
-  }
-};
-
-// [130]
-struct last_was_ns_plain_safe {
-  template <tao::yaypeg::apply_mode ApplyMode,
-            tao::yaypeg::rewind_mode RewindMode,
-            template <typename...> class Action,
-            template <typename...> class Control, typename Input>
-  static bool match(Input &, State &state) {
-    return state.lastRuleWasNsChar();
-  }
-};
-struct ns_plain_char : sor<seq<not_at<one<':', '#'>>, ns_plain_safe>,
-                           seq<last_was_ns_plain_safe, one<'#'>>,
-                           seq<one<':'>, at<ns_plain_safe>>> {};
-
-// [132]
-struct nb_ns_plain_in_line : star<seq<star<s_white>>, ns_plain_char> {};
-// [133]
-struct ns_plain_one_line : seq<ns_plain_first, nb_ns_plain_in_line> {};
-
-struct plain_scalar : plus<ns_char> {};
-struct node : until<eof, seq<plain_scalar, opt<b_line_feed>>> {};
 struct yaml : seq<push_indent> {};
 
 // ===========
@@ -236,39 +119,8 @@ template <typename Rule> struct base {
 };
 template <typename Rule> struct action : base<Rule> {};
 
-/** This struct contains an action for `ns_char`. */
-template <> struct action<ns_char> {
-
-  /**
-   * @brief PEGTL will call this function after it matched a plain scalar.
-   *
-   * @param state This parameter stores the current state of the parser.
-   */
-  template <typename Input> static void apply(const Input &, State &state) {
-    LOG("Current input matched rule `ns_char`");
-    state.setLastRuleWasNsChar(true);
-  }
-};
-
 template <> struct action<pop_indent> {
   static void apply0(State &state) { state.indentation.pop_back(); }
-};
-
-/** This struct contains an action for plain scalars. */
-template <> struct action<plain_scalar> : base<plain_scalar> {
-
-  /**
-   * @brief PEGTL will call this function after it matched a plain scalar.
-   *
-   * @param input This parameter stores the input matched by the grammar rule.
-   * @param state This parameter stores data the parser uses to create a key
-   *              set from the YAML data.
-   */
-  template <typename Input>
-  static void apply(const Input &input, State &state) {
-    LOGF("Matched plain scalar with value “{}”", input.string());
-    state.appendKey(input.string());
-  }
 };
 
 } // namespace yaypeg
