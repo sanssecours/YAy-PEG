6d8a080783d0316a28b020f3f636cc4afe5cb654
René Schwaiger <sanssecours@me.com>
2018-12-19 12:31:14 +0100

    Context: Improve string representation

--- Source/context.cpp
+++ Source/context.cpp
@@ -11,19 +11,28 @@
 
 #include "context.hpp"
 
+#include <numeric>
+
 // -- Class --------------------------------------------------------------------
 
 namespace yaypeg {
 
+using std::accumulate;
+using std::move;
+using std::next;
 using std::string;
 using std::to_string;
 
 string Context::toString() const noexcept {
-  string result = "[ ";
-  for (auto const &indent : indentation) {
-    result += to_string(indent) + " ";
-  }
-  return result + " ]";
+  string result =
+      indentation.size() <= 0
+          ? ""
+          : accumulate(next(indentation.begin()), indentation.end(),
+                       to_string(indentation[0]), // start with first element
+                       [](string text, size_t indent) {
+                         return move(text) + ", " + to_string(indent);
+                       });
+  return "[" + result + "]";
 }
 
 } // namespace yaypeg
