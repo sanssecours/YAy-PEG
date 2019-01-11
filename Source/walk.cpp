1bd419808b2867fde64382ef02c7ce1b8ff2f214
René Schwaiger <sanssecours@me.com>
2019-01-11 11:54:30 +0100

    Walk: Add initial version of “walk” function

---
+++ Source/walk.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file
+ *
+ * @brief This file contains a tree walker function.
+ *
+ * @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
+ */
+
+// -- Imports ------------------------------------------------------------------
+
+#include "walk.hpp"
+#include "listener.hpp"
+
+using tao::TAO_PEGTL_NAMESPACE::parse_tree::node;
+
+// -- Functions ----------------------------------------------------------------
+
+/**
+ * @brief This function walks a syntax tree calling methods of the given
+ *        listener.
+ *
+ * @param listener This argument specifies the listener which this function
+ *                 uses to convert the syntax tree to a key set.
+ * @param root This variable stores the root of the tree this function
+ *             visits.
+ */
+void walk(Listener &listener __attribute__((unused)),
+          node const root __attribute__((unused))) {}
---
+++ Source/walk.hpp
@@ -0,0 +1,34 @@
+/**
+ * @file
+ *
+ * @brief This file contains the declaration of a tree walker function.
+ *
+ * @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
+ */
+
+#ifndef ELEKTRA_PLUGIN_YAYPEG_WALK_HPP
+#define ELEKTRA_PLUGIN_YAYPEG_WALK_HPP
+
+// -- Imports ------------------------------------------------------------------
+
+#include "listener.hpp"
+
+#define TAO_PEGTL_NAMESPACE yaypeg
+
+#include <tao/pegtl/contrib/parse_tree.hpp>
+
+// -- Function -----------------------------------------------------------------
+
+/**
+ * @brief This function walks a syntax tree calling methods of the given
+ *        listener.
+ *
+ * @param listener This argument specifies the listener which this function
+ *                 uses to convert the syntax tree to a key set.
+ * @param root This variable stores the root of the tree this function visits.
+ */
+void walk(Listener &listener __attribute__((unused)),
+          tao::TAO_PEGTL_NAMESPACE::parse_tree::node const
+          __attribute__((unused)));
+
+#endif // ELEKTRA_PLUGIN_YAYPEG_WALK_HPP
