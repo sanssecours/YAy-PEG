10817e643e6c6b00be9c37f67e12cab89b8b34c8
René Schwaiger <sanssecours@me.com>
2019-01-12 13:18:00 +0100

    Walk: Add function to convert tree to string

--- Source/walk.cpp
+++ Source/walk.cpp
@@ -8,12 +8,45 @@
 
 // -- Imports ------------------------------------------------------------------
 
-#include "walk.hpp"
+#include <iostream>
+
 #include "listener.hpp"
+#include "walk.hpp"
+
+using std::cout;
+using std::endl;
+using std::string;
 
 using tao::TAO_PEGTL_NAMESPACE::parse_tree::node;
 
 // -- Functions ----------------------------------------------------------------
+
+namespace {
+
+/**
+ * @brief This function returns the string representation of a tree node.
+ *
+ * @param node This argument stores the tree node that this function converts to
+ *             a string.
+ * @param indent This variable stores the string representation of the current
+ *               depth of the node.
+ *
+ * @return A string representation of the given node
+ */
+string toString(node const &node, string const indent = "") {
+  string representation;
+
+  representation += node.is_root() ? "Root" : indent + node.name();
+
+  if (!node.children.empty()) {
+    for (auto &child : node.children) {
+      representation += "\n" + toString(*child, indent + "  ");
+    }
+  }
+  return representation;
+}
+
+} // namespace
 
 /**
  * @brief This function walks a syntax tree calling methods of the given
@@ -24,5 +57,14 @@ using tao::TAO_PEGTL_NAMESPACE::parse_tree::node;
  * @param root This variable stores the root of the tree this function
  *             visits.
  */
-void walk(Listener &listener __attribute__((unused)),
-          node const &root __attribute__((unused))) {}
+void walk(Listener &listener, node const &node) {
+  if (node.is_root()) {
+    cout << toString(node) << endl;
+  }
+
+  if (!node.children.empty()) {
+    for (auto &child : node.children) {
+      walk(listener, *child);
+    }
+  }
+}
